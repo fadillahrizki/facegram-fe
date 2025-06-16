d2b349b39fda2c61dd99cf75147270821f69b634
Rizky Fadillah <fadillahrizki16@gmail.com>
2025-06-17 03:45:58 +0700

    add env

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = localStorage.getItem('token');
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
