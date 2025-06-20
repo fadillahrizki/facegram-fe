787ef4405fcc39b330700d802db027650f25884c
Rizky Fadillah <fadillahrizki16@gmail.com>
2025-06-20 13:23:14 +0700

    add interceptor

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -11,4 +11,17 @@ const api = axios.create({
   }
 });
 
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async function (error) {
+    if (error.response.status == 403 || error.response.status == 401) {
+      localStorage.clear()
+      location.reload()
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default api;
