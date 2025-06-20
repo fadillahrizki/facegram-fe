7a0c3f57a5688fdecff3d8de646b011afd573041
Rizky Fadillah <fadillahrizki16@gmail.com>
2025-06-20 12:19:41 +0700

    add comment feature

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,10 @@
 import { HeartIcon as HeartSolid } from "@heroicons/react/24/solid";
-import { HeartIcon as HeartOutline } from "@heroicons/react/24/outline";
+import { ChatBubbleOvalLeftIcon, HeartIcon as HeartOutline } from "@heroicons/react/24/outline";
 import Carousel from "react-multi-carousel";
 import { Link } from "react-router";
 import api from "../api/api";
 import { useState } from "react";
+import Loading from "./Loading";
 
 export default function Post({ post }) {
   const responsive = {
@@ -26,8 +27,10 @@ export default function Post({ post }) {
   };
 
   const [user] = useState(JSON.parse(localStorage.getItem("user")))
-
+  const [content, setContent] = useState('');
+  const [loading, setLoading] = useState(false);
   const [liked, setLiked] = useState(post.likes.find(like=>like.user_id==user.id))
+  const [showComment, setShowComment] = useState(false)
 
   const handleLike = async () => {
     try {
@@ -48,33 +51,77 @@ export default function Post({ post }) {
   function kFormatter(num) {
     return Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'K' : Math.sign(num)*Math.abs(num)
   }
+
+  const handleComment = async () => {
+    setLoading(true)
+    try {
+      const res = await api.put(`posts/comment/${post.id}`, {content})
+      if(res.data.data != null) {
+        post.comments = [...post.comments, res.data.data]
+      }
+      setContent('')
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setLoading(false)
+    }
+  }
     
   return (
-    <div className="bg-white border border-gray-200 rounded-lg shadow-sm">
-        <Carousel responsive={responsive}>
-            {post.attachments.map((attachment, index) => (
-                <img
-                    key={index}
-                    src={`${import.meta.env.VITE_STORAGE_URL}/${attachment.storage_path}`}
-                    alt={`Post attachment ${index + 1}`}
-                    className="rounded-t-lg w-full max-h-96 object-cover"
-                    loading="lazy"
-                />
-            ))}
-        </Carousel>
-        <div className="p-5">
-            <div className="flex gap-2 mb-2">
-              <div className="flex gap-1 items-center text-sm text-gray-800" onClick={handleLike}> 
-                {liked ? <HeartSolid className="w-6 h-6" />  : <HeartOutline className="w-6 h-6" /> }
-                <span>{post.likes.length>0 ? kFormatter(post.likes.length) : ''}</span> 
+    <>
+      <div className="bg-white border border-gray-200 rounded-lg shadow-sm">
+          <Carousel responsive={responsive}>
+              {post.attachments.map((attachment, index) => (
+                  <img
+                      key={index}
+                      src={`${import.meta.env.VITE_STORAGE_URL}/${attachment.storage_path}`}
+                      alt={`Post attachment ${index + 1}`}
+                      className="rounded-t-lg w-full max-h-96 object-cover"
+                      loading="lazy"
+                  />
+              ))}
+          </Carousel>
+          <div className="p-5">
+              <div className="flex gap-2 mb-2">
+                <div className="flex gap-1 items-center text-sm text-gray-800 cursor-pointer" onClick={handleLike}> 
+                  {liked ? <HeartSolid className="w-6 h-6" />  : <HeartOutline className="w-6 h-6" /> }
+                  <span>{post.likes.length>0 ? kFormatter(post.likes.length) : ''}</span> 
+                </div>
+                <div className="flex gap-1 items-center text-sm text-gray-800 cursor-pointer" onClick={()=>setShowComment(!showComment)}> 
+                  <ChatBubbleOvalLeftIcon className="w-6 h-6" />
+                  <span>{post.comments.length>0 ? kFormatter(post.comments.length) : ''}</span> 
+                </div>
               </div>
-              {/* <div className="flex gap-1 items-center text-sm"> <ChatBubbleOvalLeftIcon className="w-6 h-6" /> <span>123</span> </div> */}
-            </div>
-            <Link to={`/profile/${post.user.username}`} className="font-medium hover:underline">
-                {post.user.username}
-            </Link>
-            <p className="font-normal text-gray-700">{post.caption}</p>
+              <Link to={`/profile/${post.user.username}`} className="font-medium hover:underline">
+                  {post.user.username}
+              </Link>
+              <p className="font-normal text-gray-700">{post.caption}</p>
+          </div>
+      </div>
+      {showComment && (
+      <div className="bg-white border border-gray-200 rounded-lg shadow-sm mt-2">
+        <ul className="divide-y divide-gray-200">
+          {post.comments.map(comment => (
+              <li key={comment.user.id}>
+                  <div className="flex flex-col gap-1 p-3 text-sm">
+                      <Link to={`/profile/${comment.user.username}`} className="font-medium text-gray-900 max-w-min">{comment.user.full_name}</Link>
+                      <p>{comment.content}</p>
+                  </div>
+              </li>
+          ))}
+        </ul>
+        <div className="p-3">
+          <textarea id="caption" name='caption' rows="4" className="p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Write your comments here..." value={content} onChange={(e) => setContent(e.target.value)} />
+          <button
+            className={"rounded-md bg-blue-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 " + (loading ? "cursor-not-allowed" : "cursor-pointer")}
+            disabled={loading}
+            onClick={handleComment}
+          >
+            {loading ? <Loading /> : 'Comment'}
+          </button>
         </div>
-    </div>
+      </div>
+      )}
+    </>
   );
 }
